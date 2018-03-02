00534a15b79cccd6860e0c3ecdd44373d5d42642
L_underscore <1696229469@qq.com>
2018-03-02 20:26:05 +0800

    添加product_img表

---
+++ src/main/java/com/ou/mall/bean/ProductImg.java
@@ -0,0 +1,46 @@
+package com.ou.mall.bean;
+
+public class ProductImg {
+    private Integer id;
+
+    private String img;
+
+    private String pid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid == null ? null : pid.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", img=").append(img);
+        sb.append(", pid=").append(pid);
+        sb.append("]");
+        return sb.toString();
+    }
+}
