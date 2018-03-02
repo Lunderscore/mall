83b3dd31034d40d75de6e20527654c735479fdd0
L_underscore <1696229469@qq.com>
2018-03-02 23:17:20 +0800

    ProductImgService 功能完成

--- src/main/java/com/ou/mall/service/ProductImgService.java
+++ src/main/java/com/ou/mall/service/ProductImgService.java
@@ -1,9 +1,11 @@
 package com.ou.mall.service;
 
+import com.ou.mall.bean.ProductImg;
 import com.ou.mall.exception.ErrorImgCountException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author: kpkym
@@ -12,6 +14,18 @@ import java.io.IOException;
  */
 public interface ProductImgService {
     /**
+     * @param pid 商品id
+     * @return 该商品图片list
+     */
+    List<ProductImg> listProductImg(Integer pid);
+
+    /**
+     * @param piid 商品ProductImg id
+     * @return 该商品图片
+     */
+    ProductImg getProductImg(Integer piid);
+
+    /**
      * @param file 需要保存的图片
      * @param dir  保存的目录
      * @param pid  商品id
@@ -19,4 +33,11 @@ public interface ProductImgService {
      * @throws ErrorImgCountException
      */
     void insertProductImg(MultipartFile file, String dir, Integer pid) throws IOException, ErrorImgCountException;
+
+    /**
+     * @param file 需要修改的新图片
+     * @param dir 保存的目录
+     * @param piid ProductImg id
+     */
+    void updateProductImg(MultipartFile file, String dir, Integer piid);
 }
--- src/main/java/com/ou/mall/service/impl/ProductImgServiceImpl.java
+++ src/main/java/com/ou/mall/service/impl/ProductImgServiceImpl.java
@@ -5,6 +5,7 @@ import com.ou.mall.bean.ProductImgExample;
 import com.ou.mall.dao.ProductImgMapper;
 import com.ou.mall.exception.ErrorImgCountException;
 import com.ou.mall.service.ProductImgService;
+import org.mybatis.generator.codegen.ibatis2.model.ExampleGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -26,6 +28,20 @@ public class ProductImgServiceImpl implements ProductImgService {
 
     @Value("${productImg.maxImg}")
     Integer maxImg;
+
+    @Override
+    public List<ProductImg> listProductImg(Integer pid) {
+        ProductImgExample example = new ProductImgExample();
+        ProductImgExample.Criteria criteria = example.createCriteria();
+        criteria.andPidEqualTo(pid);
+        List<ProductImg> productImgs = productImgMapper.selectByExample(example);
+        return productImgs;
+    }
+
+    @Override
+    public ProductImg getProductImg(Integer piid) {
+        return productImgMapper.selectByPrimaryKey(piid);
+    }
 
     @Override
     public void insertProductImg(MultipartFile file, String dir, Integer pid) throws IOException, ErrorImgCountException {
@@ -48,5 +64,21 @@ public class ProductImgServiceImpl implements ProductImgService {
         // 先插入数据库 后保存图片
         productImgMapper.insert(productImg);
         file.transferTo(new File(fileLocation));
+    }
+
+    @Override
+    public void updateProductImg(MultipartFile file, String dir, Integer piid) {
+        ProductImg productImg = getProductImg(piid);
+        // 拿出原来的图片
+        String databaseImg = productImg.getImg();
+        // 根据正则表达式切割字符串取出后缀名
+        String[] split = file.getOriginalFilename().split("\\.");
+        String suffix = split[split.length - 1];
+        String uuid = UUID.randomUUID().toString();
+        String fileLocation = dir + File.separator + uuid + "." + suffix;
+        productImg.setImg(fileLocation);
+        productImgMapper.updateByPrimaryKey(productImg);
+        // 删除原来的图片
+        new File(databaseImg).delete();
     }
 }
